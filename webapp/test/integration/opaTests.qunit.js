17b8ef62d3e0e4aa1df8ca5f960104b92270b0d2
aarif <aarifkhan150421.com>
2024-10-20 05:48:57 +0000

    Firts commit for git sup

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gitsup/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"gitsup/test/unit/controller/myview.controller"
+], function () {
+	"use strict";
+});
